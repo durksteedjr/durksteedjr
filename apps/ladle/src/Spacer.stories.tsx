c76b3e863fb54733a10d46fb37ead30f6afcb6d9
durksteedjr <durksteedjr@proton.me>
2023-07-17 23:56:19 -0400

    feat(apps/ladle): update app

---
+++ apps/ladle/src/Spacer.stories.tsx
@@ -0,0 +1,11 @@
+import { Flex, Spacer } from "@durksteedjr/ui";
+
+import { StyledBox } from "./components";
+
+export const Example = () => (
+  <Flex>
+    <StyledBox />
+    <Spacer />
+    <StyledBox />
+  </Flex>
+);
