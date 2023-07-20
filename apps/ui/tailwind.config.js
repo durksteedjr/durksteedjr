8c0a22fe83daa5ae8a20795eb2d8adb5eca0f7cd
durksteedjr <durksteedjr@proton.me>
2023-07-20 17:33:36 -0400

    feat(apps/ui): update app

--- apps/ui/tailwind.config.js
+++ apps/ui/tailwind.config.js
@@ -1,28 +1,9 @@
-const colors = require("@durksteedjr/colors/colors.json");
-
 /** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
-    "../../packages/ui/dist/**/*.mjs",
-    "./src/**/*.{js,jsx,mdx,ts,tsx}"
+    "./src/**/*.{js,jsx,mdx,ts,tsx}",
+    "../../durksteedjr/ui/dist/**/*.mjs",
+    "../../services/ui/**/*.{ts,tsx}"
   ],
-  theme: {
-    extend: {
-      colors: {
-        black: colors.black,
-        blue: colors.blue,
-        cyan: colors.cyan,
-        gray: colors.gray,
-        green: colors.green,
-        indigo: colors.indigo,
-        orange: colors.orange,
-        pink: colors.pink,
-        purple: colors.purple,
-        red: colors.red,
-        teal: colors.teal,
-        white: colors.white,
-        yellow: colors.yellow
-      }
-    }
-  }
+  presets: ["@configs/tailwindcss/preset.js"]
 };
