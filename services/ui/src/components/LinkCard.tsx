28ba4080a43bec2cb10e14fc0aff4f50703c50b7
durksteedjr <durksteedjr@proton.me>
2023-07-22 01:55:33 -0400

    feat(services/ui): update service

---
+++ services/ui/src/components/LinkCard.tsx
@@ -0,0 +1,44 @@
+import { Card } from "@durksteedjr/ui";
+import clsx from "clsx";
+import Link from "next/link";
+import type { AnchorHTMLAttributes } from "react";
+import {
+  HiOutlineArrowRight,
+  HiOutlineArrowUpRight
+} from "react-icons/hi2";
+
+export interface LinkCardProps
+  extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  isOutlineArrowUpRight?: boolean;
+  isOutlineRightArrow?: boolean;
+}
+
+export const LinkCard = ({
+  children,
+  className,
+  href,
+  isOutlineArrowUpRight = false,
+  isOutlineRightArrow = false,
+  ...props
+}: LinkCardProps) => (
+  <Link href={href} {...props}>
+    <Card
+      className={clsx(
+        {
+          "group relative":
+            isOutlineArrowUpRight || isOutlineRightArrow
+        },
+        className
+      )}
+      isHover
+    >
+      {children}
+      {isOutlineArrowUpRight && (
+        <HiOutlineArrowUpRight className="absolute right-6 top-[1.375rem]" />
+      )}
+      {isOutlineRightArrow && (
+        <HiOutlineArrowRight className="absolute right-6 top-[1.375rem] hidden group-hover:inline-block" />
+      )}
+    </Card>
+  </Link>
+);
