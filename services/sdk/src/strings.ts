9d4e4ca26715e90c76591d3887ce20a15a148e54
durksteedjr <durksteedjr@proton.me>
2023-07-22 01:56:29 -0400

    feat(services/sdk): update service

--- services/sdk/src/strings.ts
+++ services/sdk/src/strings.ts
@@ -1,2 +1,10 @@
 export const capitalize = (string: string) =>
   `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
+
+export const getEnvironmentString = (strings: {
+  dev: string;
+  production: string;
+}) =>
+  process.env.NODE_ENV === "development"
+    ? strings.dev
+    : strings.production;
